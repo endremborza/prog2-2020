a1b661747bfa641147433047c109b9391130ec0b
Ádám Vig <52835042+adamvig96@users.noreply.github.com>
2020-03-08 18:50:50 +0100

    Add files via upload

--- challenge-1-hotels/solutions/base_solution/process.py
+++ challenge-1-hotels/solutions/base_solution/process.py
@@ -1,23 +1,11 @@
 import pandas as pd
-import numpy as np
 import json
+from joblib import load
 
-input_locations = json.load(open("inputs.json", "r"))
+df = pd.read_pickle('filtered.pkl')
+tree = load('tree.joblib')
+input_df = pd.read_json('inputs.json')
 
-df = pd.read_csv("filtered.csv")
-
-answers = []
-
-for place in input_locations:
-    min_distance = np.inf
-    closest_place = {}
-    for idx, row in df.iterrows():
-        distance = (
-            (place["lon"] - row["lon"]) ** 2 + (place["lat"] - row["lat"]) ** 2
-        ) ** 0.5
-        if distance < min_distance:
-            min_distance = distance
-            closest_place = row[["lon", "lat", "name"]].to_dict()
-    answers.append(closest_place.copy())
+answers = [df.iloc[int(tree.query(input_df[['lon','lat']].values[i].reshape(1, -1), k = 1)[1])].to_dict() for i in range(0,input_df.shape[0])]
 
 json.dump(answers, open("outputs.json", "w"))
